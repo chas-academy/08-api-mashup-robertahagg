1169eb8c2a8c2eafdf664d552198848630e92353
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-02-02 19:12:57 +0100

    clearing search results now works

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,7 +4,14 @@ function getSearchWord() {
 
 function searchAll() {
     // Den här funktionen kör först när man tycker på submit, se ovan.
+    clearSearch();
+
     const searchWord = getSearchWord();
     searchForSynonyms(searchWord);
     searchForImages(searchWord);
+}
+
+function clearSearch() {
+    $(".relatedWords").empty();
+    $(".flickrImages").empty();
 }
